1de17d5fe50f9701cb13c70d3b87a74cea5c1380
Beharudin <beharudinmm@gmail.com>
2023-08-15 16:09:27 +0300

    Some changes

---
+++ src/components/data/MockData.js
@@ -0,0 +1,109 @@
+const eduData = [
+  {
+    title: "BTech in Information Technology",
+    school: "Veermata Jijabai Technological Institute (2020 - 2024)",
+    grade: "96.3%",
+    desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
+  },
+  {
+    title: "Higher Secondary Education",
+    school: "Rustomjee International School and Junior College (2018 - 2020)",
+    grade: "88.3%",
+    desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
+  },
+  {
+    title: "Primary & Secondary School Education",
+    school: "Rustomjee International School (2005 - 2018)",
+    grade: "96.3%",
+    desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
+  },
+];
+const expData = [
+  {
+    title: "BTech in Information Technology",
+    school: "Veermata Jijabai Technological Institute (2020 - 2024)",
+    grade: "96.3%",
+    desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
+  },
+  {
+    title: "Higher Secondary Education",
+    school: "Rustomjee International School and Junior College (2018 - 2020)",
+    grade: "88.3%",
+    desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
+  },
+  {
+    title: "Primary & Secondary School Education",
+    school: "Rustomjee International School (2005 - 2018)",
+    grade: "96.3%",
+    desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
+  },
+];
+const achvData = [
+  {
+    title: "BTech in Information Technology",
+    school: "Veermata Jijabai Technological Institute (2020 - 2024)",
+    grade: "96.3%",
+    desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
+  },
+  {
+    title: "Higher Secondary Education",
+    school: "Rustomjee International School and Junior College (2018 - 2020)",
+    grade: "88.3%",
+    desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
+  },
+  {
+    title: "Primary & Secondary School Education",
+    school: "Rustomjee International School (2005 - 2018)",
+    grade: "96.3%",
+    desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
+  },
+];
+
+const skillsData = [
+  {
+    title: "React Js",
+    value: "80",
+  },
+  {
+    title: "Node Js, EXPRESS",
+    value: "80",
+  },
+  {
+    title: "PYTHON",
+    value: "85",
+  },
+  {
+    title: "C, C++",
+    value: "85",
+  },
+  {
+    title: "HTML, CSS, JS, BOOTSTRAP",
+    value: "90",
+  },
+  {
+    title: "MONGODB & (SQL | MYSQL)",
+    value: "85",
+  },
+  {
+    title: "GIT & GITHUB",
+    value: "85",
+  },
+  {
+    title: "Java",
+    value: "80",
+  },
+  {
+    title: "Django",
+    value: "75",
+  },
+  {
+    title: "PHP",
+    value: "75",
+  },
+  {
+    title: "POSTMAN API",
+    value: "85",
+  },
+];
+
+export { eduData, expData, achvData, skillsData };
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,116 +2,14 @@ import React, { useState } from "react";
 import "./resume.css";
 import ResumeCards from "../cards/ResumeCards";
 import Skills from "../skills/Skills";
+import { eduData, expData, achvData, skillsData } from "../data/MockData";
 
 function Resume() {
   const [isActive, setIsActive] = useState("education");
-  const [educations, setEducations] = useState([
-    {
-      title: "BTech in Information Technology",
-      school: "Veermata Jijabai Technological Institute (2020 - 2024)",
-      grade: "96.3%",
-      desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
-    },
-    {
-      title: "Higher Secondary Education",
-      school: "Rustomjee International School and Junior College (2018 - 2020)",
-      grade: "88.3%",
-      desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
-    },
-    {
-      title: "Primary & Secondary School Education",
-      school: "Rustomjee International School (2005 - 2018)",
-      grade: "96.3%",
-      desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
-    },
-  ]);
-  const [experience, setExperience] = useState([
-    {
-      title: "BTech in Information Technology",
-      school: "Veermata Jijabai Technological Institute (2020 - 2024)",
-      grade: "96.3%",
-      desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
-    },
-    {
-      title: "Higher Secondary Education",
-      school: "Rustomjee International School and Junior College (2018 - 2020)",
-      grade: "88.3%",
-      desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
-    },
-    {
-      title: "Primary & Secondary School Education",
-      school: "Rustomjee International School (2005 - 2018)",
-      grade: "96.3%",
-      desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
-    },
-  ]);
-  const [achievements, setAchievements] = useState([
-    {
-      title: "BTech in Information Technology",
-      school: "Veermata Jijabai Technological Institute (2020 - 2024)",
-      grade: "96.3%",
-      desc: "Studied IT related subjects like Operating Systems, DBMS,OOPS, ML, Web Development, AI, DSA, Compiler Design etc.",
-    },
-    {
-      title: "Higher Secondary Education",
-      school: "Rustomjee International School and Junior College (2018 - 2020)",
-      grade: "88.3%",
-      desc: "Studied in this school from 11th to 12th std. HSC Subjects: PCM, English, Electrical Maintenance, Environment Education.",
-    },
-    {
-      title: "Primary & Secondary School Education",
-      school: "Rustomjee International School (2005 - 2018)",
-      grade: "96.3%",
-      desc: "Studied in this school from Nursery to 10th std. SSC Subjects: English, Hindi-French, Marathi, Maths, Science, Social Sciences.",
-    },
-  ]);
-
-  const [skills, setSkills] = useState([
-    {
-      title: 'React Js',
-      value: '80'
-    },
-    {
-      title: 'Node Js, EXPRESS',
-      value: '80'
-    },
-    {
-      title: 'PYTHON',
-      value: '85'
-    },
-    {
-      title: 'C, C++',
-      value: '85'
-    },
-    {
-      title: 'HTML, CSS, JS, BOOTSTRAP',
-      value: '90'
-    },
-    {
-      title: 'MONGODB & (SQL | MYSQL)',
-      value: '85'
-    },
-    {
-      title: 'GIT & GITHUB',
-      value: '85'
-    },
-    {
-      title: 'Java',
-      value: '80'
-    },
-    {
-      title: 'Django',
-      value: '75'
-    },
-    {
-      title: 'PHP',
-      value: '75'
-    },
-    {
-      title: 'POSTMAN API',
-      value: '85'
-    }
-  ])
+  const [educations, setEducations] = useState(eduData);
+  const [experience, setExperience] = useState(expData);
+  const [achievements, setAchievements] = useState(achvData);
+  const [skills, setSkills] = useState(skillsData);
 
   return (
     <div className="fluid-container" id="resume">
@@ -191,18 +89,22 @@ function Resume() {
                     <h1 className="resumeTitleDesc">Development Skill</h1>
                   </div>
                 </div>
-                {<Skills data={skills.slice(0, Math.ceil(skills.length / 2))}/>}
+                {
+                  <Skills
+                    data={skills.slice(0, Math.ceil(skills.length / 2))}
+                  />
+                }
               </div>
             </div>
             <div className="col-12 col-md-6 mb-5">
               <div className="row">
-              <div className="col-12 mb-5">
+                <div className="col-12 mb-5">
                   <div className="resumeTitleDiv">
                     <p className="resumeTitle">TECH STACK</p>
                     <h1 className="resumeTitleDesc">Development Skill</h1>
                   </div>
                 </div>
-                {<Skills  data={skills.slice(Math.ceil(skills.length / 2))}/>}
+                {<Skills data={skills.slice(Math.ceil(skills.length / 2))} />}
               </div>
             </div>
           </>
@@ -240,7 +142,10 @@ function Resume() {
               <div className="row">
                 {
                   <ResumeCards
-                    data={achievements.slice(0, Math.ceil(achievements.length / 2))}
+                    data={achievements.slice(
+                      0,
+                      Math.ceil(achievements.length / 2)
+                    )}
                     header={{
                       period: "2005 - Present",
                       title: "Accomplishments",
@@ -253,7 +158,9 @@ function Resume() {
               <div className="row">
                 {
                   <ResumeCards
-                    data={achievements.slice(Math.ceil(achievements.length / 2))}
+                    data={achievements.slice(
+                      Math.ceil(achievements.length / 2)
+                    )}
                     header={{
                       period: "2005 - Present",
                       title: "Accomplishments",
